85ee9cefde0d5283a3c6994b12c60f7161495d34
makemeunsee <marc-antoine.galilee@etik.com>
2025-01-27 00:17:38 +0100

    with the app

---
+++ pkg/web_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start: () => void;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hd156653c6b3ab891: (a: number, b: number) => void;
+export const closure17_externref_shim: (a: number, b: number, c: any) => void;
+export const closure160_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8304bd66e6215dd9: (a: number, b: number) => void;
+export const closure490_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
