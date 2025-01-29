018580798dac0240757a7738b6a304ced97902a3
makemeunsee <marc-antoine.galilee@etik.com>
2025-01-29 13:17:00 +0100

    numbers shown

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -1,5 +1,5 @@
 use log::info;
-use solvers::volume;
+use solvers::{common::Dir, volume};
 use std::collections::HashMap;
 use three_d::*;
 
@@ -14,8 +14,9 @@ pub fn demo_3d() {
 
     let mut camera = Camera::new_perspective(
         window.viewport(),
-        vec3(0.0, 10.0, 20.0),
-        vec3(0.0, 0.0, 0.0),
+        vec3(20.0, 30.0, 45.0),
+        // look at the center of the puzzle
+        vec3(12.0, 11., 9.),
         vec3(0.0, 1.0, 0.0),
         degrees(90.0),
         0.1,
@@ -23,25 +24,30 @@ pub fn demo_3d() {
     );
     let mut control = OrbitControl::new(camera.target(), 1.0, 100.0);
 
-    let text_generator = TextGenerator::new(include_bytes!("OldEnglishFive.ttf"), 0, 1.8).unwrap();
-    let text_mesh = text_generator.generate("52", TextLayoutOptions::default());
-    let mut text = Gm::new(
-        Mesh::new(&context, &text_mesh),
-        ColorMaterial {
-            color: Srgba::BLACK,
-            ..Default::default()
-        },
-    );
-    text.material.render_states.cull = Cull::Front;
+    let mut numbers = vec![];
+    let text_generator = TextGenerator::new(include_bytes!("OldEnglishFive.ttf"), 0, 2.).unwrap();
+    for i in 0..54 {
+        let text_mesh =
+            text_generator.generate(&format!("{:0>2}", i + 1), TextLayoutOptions::default());
+        let mut text = Gm::new(
+            Mesh::new(&context, &text_mesh),
+            ColorMaterial {
+                color: Srgba::BLACK,
+                ..Default::default()
+            },
+        );
+        text.material.render_states.cull = Cull::Front;
+        numbers.push(text);
+    }
 
     let mut pbox = Gm::new(
         Mesh::new(&context, &CpuMesh::cube()),
         PhysicalMaterial::new(&context, &CpuMaterial::default()),
     );
     pbox.set_transformation(
-        Mat4::from_translation(vec3(-6.0, -5.5,  -4.5)) // puzzle is 12x11x9 -> center
-            * Mat4::from_nonuniform_scale(12., 11., 9.)
-            * Mat4::from_scale(0.5)
+        // scale by the puzzle size
+        Mat4::from_nonuniform_scale(12., 11., 9.)
+            // the base cube is centered on origin, and we want the origin to be a corner of the puzzle
             * Mat4::from_translation(vec3(1., 1., 1.)),
     );
     let bounding_box = Gm::new(
@@ -70,14 +76,13 @@ pub fn demo_3d() {
     let mut solving = false;
     let mut step_once = false;
     let mut step_to_sol = false;
-
-    let mut meshes = vec![];
-    for idx in 0..solver.block_count() {
-        let color = colors[&idx];
-        meshes.push(block(&context, color.0, color.1, color.2));
-    }
+    let mut transparency = true;
+    let mut monochrome = false;
+    let mut show_numbers = false;
+    let mut shape_only = true;
 
     let mut gui = three_d::GUI::new(&context);
+
     window.render_loop(move |mut frame_input| {
         let mut panel_width = 0.0;
         gui.update(
@@ -91,6 +96,7 @@ pub fn demo_3d() {
                     use three_d::egui::*;
                     ui.add_space(50.);
                     ui.heading("Control Panel");
+                    ui.add(three_d::egui::Separator::default());
                     ui.add(Slider::new(&mut step_freq, 1..=120).text("Speed"));
                     if ui.add(Button::new("Play")).clicked() {
                         solving = true;
@@ -103,6 +109,19 @@ pub fn demo_3d() {
                         solving = false;
                     };
                     ui.add(Checkbox::new(&mut step_to_sol, "Step to solutions only"));
+                    if ui
+                        .add(Checkbox::new(
+                            &mut shape_only,
+                            "Toggle solving side values sum to 100 (restart)",
+                        ))
+                        .clicked()
+                    {
+                        // TODO
+                    }
+                    ui.add(three_d::egui::Separator::default());
+                    ui.add(Checkbox::new(&mut show_numbers, "Show numbers"));
+                    ui.add(Checkbox::new(&mut transparency, "Transparency"));
+                    ui.add(Checkbox::new(&mut monochrome, "Monochrome"));
                 });
                 panel_width = gui_context.used_rect().width();
             },
@@ -118,30 +137,102 @@ pub fn demo_3d() {
 
         control.handle_events(&mut camera, &mut frame_input.events);
 
-        for mesh in &mut meshes {
-            mesh.set_transformation(Mat4::from_translation(vec3(1., 1., -1000.)));
+        // hide the numbers away, relevant ones are placed where needed later on
+        for mesh in &mut numbers {
+            mesh.set_transformation(Mat4::from_translation(vec3(0., -1000., -1000.)));
         }
+
+        let mut blocks = vec![];
         for b in &solver.stack() {
-            let h = b.0.height as f32;
-            let w = b.0.width as f32;
-            let d = b.0.depth as f32;
-            let x = b.2 as f32;
-            let y = b.3 as f32;
-            let z = b.4 as f32;
-            let position = Mat4::from_translation(vec3(x - 6.0, y - 5.5, z - 4.5)); // puzzle is 12x11x9 -> center
-            let shape = Mat4::from_nonuniform_scale(h - 0.6, w - 0.6, d - 0.6);
-            let cube_too_big = Mat4::from_scale(0.5);
+            let color = if monochrome {
+                (100, 100, 80)
+            } else {
+                colors[&b.1]
+            };
+            let mut block = block_gm(&context, color.0, color.1, color.2, transparency);
+
+            // small GAP between the blocks; prettier and decreases transparency issues
+            const GAP: f32 = 0.8;
+            let h = b.0.height as f32 - GAP;
+            let w = b.0.width as f32 - GAP;
+            let d = b.0.depth as f32 - GAP;
+            let shape = Mat4::from_nonuniform_scale(h, w, d);
+
+            // factor 2 as the base cube is 2x2x2
+            let x = 2. * b.2 as f32 + GAP;
+            let y = 2. * b.3 as f32 + GAP;
+            let z = 2. * b.4 as f32 + GAP;
+            let position = Mat4::from_translation(vec3(x, y, z));
+
+            // the base cube is centered on origin, and we want the origin to be a corner of the puzzle
             let cube_offset = Mat4::from_translation(vec3(1., 1., 1.));
-            meshes[b.1].set_transformation(position * shape * cube_too_big * cube_offset);
+            block.set_transformation(position * shape * cube_offset);
+            blocks.push(block);
+
+            // used to roughly center the number on the face; measured, varies with font size
+            const TEXT_HALF_WIDTH: f32 = 1.3;
+            const TEXT_HALF_HEIGHT: f32 = 0.9;
+            // so the numbers are an epsilon in front of the face and visible
+            const EPS: f32 = 0.01;
+
+            if show_numbers {
+                for face in &b.0.faces {
+                    let mesh = &mut numbers[face.value as usize - 1];
+                    let trans = match face.dir {
+                        Dir::Front => Mat4::from_translation(vec3(
+                            x + h - TEXT_HALF_WIDTH,
+                            y + w - TEXT_HALF_HEIGHT,
+                            z + 2. * d + EPS,
+                        )),
+                        Dir::Back => {
+                            Mat4::from_translation(vec3(
+                                x + h + TEXT_HALF_WIDTH,
+                                y + w - TEXT_HALF_HEIGHT,
+                                z - EPS,
+                            )) * Mat4::from_angle_y(Deg(180.))
+                        }
+                        Dir::Left => {
+                            Mat4::from_translation(vec3(
+                                x + h - TEXT_HALF_WIDTH,
+                                y + 2. * w + EPS,
+                                z + d + TEXT_HALF_HEIGHT,
+                            )) * Mat4::from_angle_x(Deg(-90.))
+                        }
+                        Dir::Right => {
+                            Mat4::from_translation(vec3(
+                                x + h - TEXT_HALF_WIDTH,
+                                y - EPS,
+                                z + d - TEXT_HALF_HEIGHT,
+                            )) * Mat4::from_angle_x(Deg(90.))
+                        }
+                        Dir::Top => {
+                            Mat4::from_translation(vec3(
+                                x + 2. * h + EPS,
+                                y + w - TEXT_HALF_HEIGHT,
+                                z + d + TEXT_HALF_WIDTH,
+                            )) * Mat4::from_angle_y(Deg(90.))
+                        }
+                        Dir::Bottom => {
+                            Mat4::from_translation(vec3(
+                                x - EPS,
+                                y + w - TEXT_HALF_HEIGHT,
+                                z + d - TEXT_HALF_WIDTH,
+                            )) * Mat4::from_angle_y(Deg(-90.))
+                        }
+                    };
+                    mesh.set_transformation(trans);
+                }
+            }
         }
 
         frame_input
             .screen()
             .clear(ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0))
-            .render(&camera, &meshes, &[]);
-        frame_input
-            .screen()
-            .render(&camera, bounding_box.into_iter().chain(&text), &[]);
+            .render(&camera, &blocks, &[]);
+        if show_numbers {
+            frame_input.screen().render(&camera, &numbers, &[]);
+        }
+        frame_input.screen().render(&camera, &bounding_box, &[]);
         frame_input.screen().write(|| gui.render()).unwrap();
 
         if solving || step_once {
@@ -164,16 +255,41 @@ pub fn demo_3d() {
     });
 }
 
-fn block(context: &Context, r: u8, g: u8, b: u8) -> Gm<Mesh, PhysicalMaterial> {
+fn block_gm(
+    context: &Context,
+    r: u8,
+    g: u8,
+    b: u8,
+    transparency: bool,
+) -> Gm<Mesh, PhysicalMaterial> {
     Gm::new(
         Mesh::new(context, &CpuMesh::cube()),
-        PhysicalMaterial::new_transparent(
-            context,
-            &CpuMaterial {
-                albedo: Srgba::new(r, g, b, 128),
-                emissive: Srgba::new(r, g, b, 50),
-                ..Default::default()
-            },
-        ),
+        if transparency {
+            transparent_mat(context, r, g, b)
+        } else {
+            opaque_mat(context, r, g, b)
+        },
+    )
+}
+
+fn transparent_mat(context: &Context, r: u8, g: u8, b: u8) -> PhysicalMaterial {
+    PhysicalMaterial::new_transparent(
+        context,
+        &CpuMaterial {
+            albedo: Srgba::new(r, g, b, 128),
+            emissive: Srgba::new(r, g, b, 50),
+            ..Default::default()
+        },
+    )
+}
+
+fn opaque_mat(context: &Context, r: u8, g: u8, b: u8) -> PhysicalMaterial {
+    PhysicalMaterial::new_opaque(
+        context,
+        &CpuMaterial {
+            albedo: Srgba::new(r, g, b, 128),
+            emissive: Srgba::new(r, g, b, 50),
+            ..Default::default()
+        },
     )
 }
--- solvers/src/lib.rs
+++ solvers/src/lib.rs
@@ -1,3 +1,3 @@
-mod common;
+pub mod common;
 pub mod constraints;
 pub mod volume;
