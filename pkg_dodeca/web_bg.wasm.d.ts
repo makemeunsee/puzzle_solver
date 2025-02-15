67716fa4f4b399893b7e12636fcf90007d256533
makemeunsee <marc-antoine.galilee@etik.com>
2025-02-15 13:23:39 +0100

    wip dodeca demo

---
+++ pkg_dodeca/web_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start: () => void;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure42_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h41f7c06f81a4eca8: (a: number, b: number) => void;
+export const closure162_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0569de40c0eeabcf: (a: number, b: number) => void;
+export const closure501_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
