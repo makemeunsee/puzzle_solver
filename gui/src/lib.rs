51d1c29c8d36be1cd85717abd667d67cb7bab03f
makemeunsee <marc-antoine.galilee@etik.com>
2025-03-08 23:44:00 +0100

    (un)scramble

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -1,24 +1,29 @@
 mod shapes;
 
+use web_time::SystemTime;
+
 use cgmath::{Angle, Matrix};
-use egui_commonmark::{CommonMarkCache, CommonMarkViewer};
 use itertools::Itertools;
+use rand::{rngs::SmallRng, RngCore, SeedableRng};
 use shapes::{
     facet_shift_rotation, Polyhedron, FACET0_CENTER, FACET_COUNT, ICO_TILE_COUNT, TILE0_CENTER,
     TILES_CENTERS, TRANSFORMATIONS_BASE,
 };
 use solvers::dodeca::{offset_to_rot, FACETS, PENTAS_GRAPH, TRI_TO_FACETS};
 use three_d::{
-    core::Context, degrees, pick, vec3, AmbientLight, Attenuation, Camera, ClearState, Cull,
-    DirectionalLight, Event, FrameOutput, FreeOrbitControl, Gm, InnerSpace, InstancedMesh,
-    Instances, Light, Mat4, Mesh, MouseButton, Object, PhysicalMaterial, PointLight, RendererError,
-    Srgba, TextGenerator, TextLayoutOptions, Vec3, Vec4, Viewport, Window, WindowSettings,
+    core::Context, degrees, pick, vec3, AmbientLight, Attenuation, Camera, ClearState,
+    ColorMaterial, Cull, DirectionalLight, Event, FrameOutput, FreeOrbitControl, Gm, InnerSpace,
+    InstancedMesh, Instances, Light, Mat4, Mesh, MouseButton, Object, PhysicalMaterial, PointLight,
+    RendererError, Srgba, TextGenerator, TextLayoutOptions, Vec3, Vec4, Viewport, Window,
+    WindowSettings,
 };
 
 const COLOR_LIGHT_BLUE: Srgba = Srgba::new_opaque(100, 150, 255);
 const COLOR_LIGHT_GOLD: Srgba = Srgba::new_opaque(220, 210, 140);
 const COLOR_NEON_GREEN: Srgba = Srgba::new_opaque(100, 255, 100);
 const COLOR_FIERY_RED: Srgba = Srgba::new_opaque(255, 80, 0);
+const COLOR_FIERY_GREEN: Srgba = Srgba::new_opaque(0, 255, 80);
+const COLOR_FIERY_BLUE: Srgba = Srgba::new_opaque(80, 0, 255);
 const COLOR_GOLD: Srgba = Srgba::new_opaque(212, 175, 55);
 const COLOR_YELLOW: Srgba = Srgba::new_opaque(255, 226, 0);
 const COLOR_GRAY_BROWN: Srgba = Srgba::new_opaque(94, 94, 80);
@@ -41,13 +46,11 @@ fn font_bytes_and_size(font: Font) -> (&'static [u8], f32) {
 
 const ANCHOR_TILE_ID: usize = 11;
 
-const SEED0: u64 = 0xACE0FBA5E15DEAD;
-
 fn generate_glyphs(
     context: &Context,
     font: &[u8],
     font_size: f32,
-) -> Vec<(Gm<Mesh, PhysicalMaterial>, usize, Mat4)> {
+) -> Vec<(Gm<Mesh, ColorMaterial>, usize, Mat4)> {
     // common matrices to place numbers on facets
     let smaller = Mat4::from_scale(0.1);
     let facet_align =
@@ -59,7 +62,6 @@ fn generate_glyphs(
     let text_generator = TextGenerator::new(font, 0, font_size).unwrap();
 
     let mut glyphs = vec![];
-    //     "•", "°", "+", "=", "-", "|", ".", "…", ":", "o", "^", "�", "¦",
     let patterns = [
         ".",      // white
         "..\n..", // blue
@@ -70,8 +72,8 @@ fn generate_glyphs(
         "..",     // purple
         "…",      // teal
         "..\n..", // dark gray
-        "..",     // orange
-        "…",      // teal
+        "…",      // orange
+        "..",     // teal
         "..",     // dark gray
         ".",      // orange
         "..\n..", // gray
@@ -83,7 +85,6 @@ fn generate_glyphs(
         "..",     // white
     ];
     for (facet, (_, tri)) in FACETS.iter().enumerate() {
-        // let glyph = format!("{} {}", facet, patterns[*tri]);
         let glyph = patterns[*tri];
         let text_mesh = text_generator.generate(glyph, TextLayoutOptions { line_height: 0.2 });
         let (x_min, x_max, y_min, y_max, z_min, z_max) = text_mesh.positions.to_f32().iter().fold(
@@ -105,16 +106,8 @@ fn generate_glyphs(
         ));
         let mut text = Gm::new(
             Mesh::new(context, &text_mesh),
-            PhysicalMaterial {
-                albedo: Srgba::BLACK,
-                emissive: Srgba {
-                    r: 5,
-                    g: 5,
-                    b: 0,
-                    a: 255,
-                },
-                metallic: 0.6,
-                roughness: 0.3,
+            ColorMaterial {
+                color: Srgba::BLACK,
                 ..Default::default()
             },
         );
@@ -129,19 +122,24 @@ fn generate_glyphs(
 }
 
 pub fn demo_3d() {
-    run(Model::new());
+    let seed = SystemTime::now()
+        .duration_since(web_time::UNIX_EPOCH)
+        .unwrap()
+        .as_millis() as u64;
+    run(Model::new(seed));
 }
 
 #[derive(Clone)]
 struct Model {
-    seed: u64,
+    rng: SmallRng,
+    moves: Vec<(usize, bool)>,
 }
 
 impl Model {
-    fn new() -> Self {
-        // let mut rng = SmallRng::seed_from_u64(SEED0);
+    fn new(seed: u64) -> Self {
+        let rng = SmallRng::seed_from_u64(seed);
 
-        Model { seed: SEED0 }
+        Model { rng, moves: vec![] }
     }
 
     fn reset(&mut self) {}
@@ -155,7 +153,11 @@ struct UIState {
     new_rotation: f32,
     font: Font,
     has_changes: bool,
-    win_anim: Option<f32>,
+    unscrambling: bool,
+    unscrambling_one: bool,
+    scrambling: bool,
+    scrambling_one: bool,
+    scrambling_count: usize,
 }
 
 impl UIState {
@@ -168,7 +170,11 @@ impl UIState {
             new_rotation: 0.,
             font: Font::TypeLightSans,
             has_changes: true,
-            win_anim: None,
+            unscrambling: false,
+            scrambling: false,
+            unscrambling_one: false,
+            scrambling_one: false,
+            scrambling_count: 0,
         }
     }
 
@@ -176,7 +182,11 @@ impl UIState {
         self.picked_facet_id = None;
         self.rotating = [None; ICO_TILE_COUNT];
         self.has_changes = true;
-        self.win_anim = None;
+        self.unscrambling = false;
+        self.unscrambling_one = false;
+        self.scrambling = false;
+        self.scrambling_one = false;
+        self.scrambling_count = 0;
     }
 
     fn handle_event(
@@ -231,9 +241,6 @@ impl UIState {
     }
 
     fn handle_picking(&mut self) {
-        if self.win_anim.is_some() {
-            return;
-        }
         if let Some(pick_id) = self.new_pick {
             self.picked_facet_id = match self.picked_facet_id {
                 // picked the same facet
@@ -277,7 +284,7 @@ fn run(mut model: Model) {
         1.,
         20.0,
     );
-    camera.alt_proj = variable_projection(window.viewport().aspect(), 0.5, 45.);
+    // camera.alt_proj = variable_projection(window.viewport().aspect(), 0.5, 45.);
 
     fn facet_colors_init() -> Vec<Srgba> {
         let colors = [
@@ -285,7 +292,7 @@ fn run(mut model: Model) {
             Srgba::BLUE,
             Srgba::RED,
             Srgba::GREEN,
-            Srgba::new_opaque(178, 178, 178),
+            Srgba::new_opaque(127, 127, 127),
             Srgba::new_opaque(255, 255, 0),
             Srgba::new_opaque(255, 0, 255),
             Srgba::new_opaque(0, 255, 255),
@@ -294,7 +301,7 @@ fn run(mut model: Model) {
             Srgba::new_opaque(0, 255, 255),   //10
             Srgba::new_opaque(50, 50, 50),    //11
             Srgba::new_opaque(255, 140, 0),   //12
-            Srgba::new_opaque(178, 178, 178), //13
+            Srgba::new_opaque(127, 127, 127), //13
             Srgba::new_opaque(255, 255, 0),   //14
             Srgba::new_opaque(255, 0, 255),   //15
             Srgba::GREEN,                     //16
@@ -370,13 +377,13 @@ fn run(mut model: Model) {
         vec3(0.0, 0.0, -1.0),
         Attenuation {
             constant: 0.,
-            linear: 0.,
-            quadratic: 0.,
+            linear: 0.5,
+            quadratic: 0.05,
         },
     );
 
-    let mut directional_light =
-        DirectionalLight::new(&context, 0.7, COLOR_FIERY_RED, vec3(0.0, -1.0, 0.0));
+    let mut directional_light_r =
+        DirectionalLight::new(&context, 0.7, COLOR_FIERY_RED, vec3(-0.5, -0.86, -1.0));
 
     // rendering & animation
     let mut trans_factor = 0.05;
@@ -386,12 +393,9 @@ fn run(mut model: Model) {
     let mut control = FreeOrbitControl::new(camera.target(), 1.0, 50.0);
 
     let mut gui = three_d::GUI::new(&context);
-    let mut _seed_buffer = format!("{}", model.seed);
 
-    let mut modal_rules = false;
-
-    // let mut perspective_factor = 0.5;
-    // let mut fov_y = 45.;
+    let mut perspective_factor = 0.5;
+    let mut fov_y = 45.;
 
     window.render_loop(move |mut frame_input| {
         let mut panel_width = 0.0;
@@ -405,15 +409,38 @@ fn run(mut model: Model) {
                 SidePanel::left("side_panel").show(gui_context, |ui| {
                     use three_d::egui::*;
                     ui.add_space(50.);
-                    if ui.button("How to play").clicked() {
-                        modal_rules = true;
-                    }
+                    ui.label("Click -> select");
+                    ui.label("Wheel -> rotate");
                     // ui.add_space(50.);
                     // ui.label("projection");
                     // ui.add(Slider::new(&mut perspective_factor, 0.0..=1.0));
                     // ui.label("FOV y");
                     // ui.add(Slider::new(&mut fov_y, 0.0..=180.0));
                     ui.add_space(50.);
+                    if ui
+                        .add_enabled(
+                            !ui_state.unscrambling && !ui_state.scrambling,
+                            Button::new("Scramble"),
+                        )
+                        .clicked()
+                    {
+                        ui_state.scrambling = true;
+                        ui_state.scrambling_count = 6;
+                        ui_state.picked_facet_id = None;
+                        ui_state.has_changes = true;
+                    }
+
+                    if ui
+                        .add_enabled(
+                            !ui_state.unscrambling && !ui_state.scrambling,
+                            Button::new("Unscramble"),
+                        )
+                        .clicked()
+                    {
+                        ui_state.unscrambling = !model.moves.is_empty();
+                        ui_state.picked_facet_id = None;
+                        ui_state.has_changes = true;
+                    }
 
                     if ui.button("Reset").clicked() {
                         model.reset();
@@ -423,37 +450,6 @@ fn run(mut model: Model) {
                         facet_colors = facet_colors_init();
                         trans_factor = 0.05;
                     };
-
-                    // ui.separator();
-                    // ui.heading("Generation");
-                    // ui.text_edit_singleline(&mut seed_buffer);
-                    // if ui.button("Randomize tiles from seed").clicked() {
-                    //     model.seed = if let Ok(number) = seed_buffer.parse() {
-                    //         number
-                    //     } else {
-                    //         let mut hasher = DefaultHasher::new();
-                    //         seed_buffer.hash(&mut hasher);
-                    //         hasher.finish()
-                    //     };
-                    // }
-
-                    if modal_rules {
-                        let modal = Modal::new(Id::new("rules")).show(ui.ctx(), |ui| {
-                            ui.set_width(frame_input.viewport.width as f32 / 2.);
-
-                            let markdown = r"# How to play
-
-* `left click` to select
-* `mouse wheel` to rotate";
-
-                            let mut cache = CommonMarkCache::default();
-                            CommonMarkViewer::new().show(ui, &mut cache, markdown);
-                        });
-
-                        if modal.should_close() {
-                            modal_rules = false;
-                        }
-                    }
                 });
                 panel_width = gui_context.used_rect().width();
             },
@@ -469,11 +465,43 @@ fn run(mut model: Model) {
         camera.set_viewport(viewport);
         // camera.alt_proj = variable_projection(viewport.aspect(), perspective_factor, fov_y);
 
-        for event in frame_input.events.iter_mut() {
-            ui_state.handle_event(event, &context, &mut camera, &facets);
+        if ui_state.unscrambling {
+            if !ui_state.unscrambling_one {
+                if let Some((i, clockwise)) = model.moves.pop() {
+                    for (j, rot) in ui_state.rotating.iter_mut().enumerate() {
+                        if i == j {
+                            *rot = Some((0., !clockwise));
+                            ui_state.unscrambling_one = true;
+                        } else {
+                            *rot = None;
+                        }
+                    }
+                } else {
+                    // not required atm
+                    ui_state.unscrambling = false;
+                    ui_state.unscrambling_one = false;
+                }
+            }
+        } else if ui_state.scrambling {
+            if !ui_state.scrambling_one && ui_state.scrambling_count > 0 {
+                let i = model.rng.next_u32() as usize % 12;
+                let clockwise = model.rng.next_u32() % 2 == 0;
+                for (j, rot) in ui_state.rotating.iter_mut().enumerate() {
+                    if i == j {
+                        *rot = Some((0., clockwise));
+                        ui_state.scrambling_one = true;
+                    } else {
+                        *rot = None;
+                    }
+                }
+            }
+        } else {
+            for event in frame_input.events.iter_mut() {
+                ui_state.handle_event(event, &context, &mut camera, &facets);
+            }
+            // process all events, then handle picking if any
+            ui_state.handle_picking();
         }
-        // process all events, then handle picking if any
-        ui_state.handle_picking();
 
         // facet rotation processing
         for (i, rot_opt) in ui_state.rotating.iter_mut().enumerate() {
@@ -516,6 +544,19 @@ fn run(mut model: Model) {
                                 glyphs[offset1].1 = tmp;
                             }
                         }
+                        if ui_state.unscrambling {
+                            ui_state.unscrambling_one = false;
+                            ui_state.unscrambling = !model.moves.is_empty();
+                        } else {
+                            model.moves.push((i, clockwise));
+                            if ui_state.scrambling {
+                                ui_state.scrambling_count -= 1;
+                                ui_state.scrambling_one = false;
+                                if ui_state.scrambling_count == 0 {
+                                    ui_state.scrambling = false;
+                                }
+                            }
+                        }
 
                         ui_state.has_changes = true;
                     }
@@ -524,37 +565,18 @@ fn run(mut model: Model) {
         }
 
         // apply visual changes
-        if ui_state.win_anim.is_none() && ui_state.has_changes {
-            let win = false;
-
+        if ui_state.has_changes {
             for num in glyphs.iter_mut() {
-                num.0.material.albedo = Srgba::BLACK;
-                num.0.material.emissive = Srgba::BLACK;
+                num.0.material.color = Srgba::BLACK;
             }
             if let Some(id) = ui_state.picked_facet_id {
                 let id = id / 5;
                 for num in glyphs.iter_mut().skip(id * 5).take(5) {
-                    num.0.material.emissive = Srgba::WHITE;
-                }
-            }
-
-            if win {
-                ui_state.win_anim = Some(0.);
-                if let Some(id) = ui_state.picked_facet_id {
-                    for num in glyphs.iter_mut().skip(id * 5).take(5) {
-                        num.0.material.emissive = COLOR_FIERY_RED;
-                    }
+                    num.0.material.color = Srgba::WHITE;
                 }
             }
 
             ui_state.has_changes = false;
-        }
-        if let Some(prog) = ui_state.win_anim {
-            let delta = frame_input.elapsed_time as f32 / 10.;
-            let next_prog = f32::min(100., prog + delta);
-            ui_state.win_anim = Some(next_prog);
-
-            trans_factor *= 1. + delta / 35.;
         }
 
         // compute facet transformations (rotations, swapping animations)
@@ -607,11 +629,12 @@ fn run(mut model: Model) {
         control.handle_events(&mut camera, &mut frame_input.events);
 
         // fix lights to camera
-        let cam_pos = camera.position();
-        point_light.position = 3. * cam_pos.normalize();
-        directional_light.direction = ((-cam_pos - camera.up()) / 2.).normalize();
+        let cam_pos = camera.position().normalize();
+        let up = camera.up();
+        point_light.position = 3. * cam_pos;
+        directional_light_r.direction = -cam_pos - up;
 
-        let lights = [&ambient as &dyn Light, &point_light, &directional_light];
+        let lights = [&ambient as &dyn Light, &point_light, &directional_light_r];
 
         // draw
         let screen = frame_input.screen();
